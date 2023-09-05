4e6e739a6fdb47b338cb96f5bf1376a9571075d3
bek07kg <bekamurzaev4@gmail.com>
2023-09-05 15:52:15 +0300

    lesson_1

---
+++ app.py
@@ -0,0 +1,33 @@
+# Hello World!
+print ('Hello World!')
+
+# variables:
+print ('Variables:')
+
+# int ===> Integer
+print ('int:')
+n1 = int (1)
+n2 = int (25)
+n3 = int (2023)
+
+print (n1)
+print (n2)
+print (n3)
+
+# boolean
+print ('boolean:')
+yes = bool (True)
+no = bool (False)
+
+print (yes)
+print (no)
+
+#str ===> String
+print ('str:')
+s1 = str (2589.54)
+s2 = str ("Django")
+s3 = str (True)
+
+print (s1)
+print (s2)
+print (s3)
