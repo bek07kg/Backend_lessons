811db9d3b8342e209b81dfdce1868fc49176d4c7
bek07kg <bekamurzaev4@gmail.com>
2023-09-05 19:00:18 +0300

    python

---
+++ numbers_1.py
@@ -0,0 +1,36 @@
+number1 = 79
+number2 = str (number1)
+
+
+print (78)
+print (number2)
+print (50 + 20)
+print (120 - 48)
+print (5.5 + 5.5)
+print (360 / 6)
+print (2 * 2)
+print (2 ** 4)
+print (75 // 2)
+print (75 % 2)
+
+a = int ( input ('а га маани бер: '))
+b = int ( input ('b га маани бер: '))
+c = a + b
+print ('а менен b ны кошкондо =',+ c)
+
+print (abs(-5))
+print (abs(-0.2))
+print (max(4, 5, 9, 87, 20, 1))
+print (min(4, 5, 9, 87, 20, 1))
+print (round(3.2))
+print (round(3.6))
+print (bin(2023))
+print (bin(-1))
+print (bin(-65))
+
+
+from math import*
+
+
+print (sqrt(50))
+print (sqrt(200))
